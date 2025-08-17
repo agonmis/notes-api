d6f91fe5ced3a8a4c4179e2c0de016caa0d56d54
agonmi <agon_misiraj@outlook.de>
2025-08-17 09:55:56 +0200

    zweiter commit

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Note(BaseModel):
+    title: str
+    content: str
+    done: bool
+
+class NoteUpdate(BaseModel):
+    content: str
 
 app = FastAPI()
 
+notes = []
+
+@app.post("/notes")
+async def create_note(request: Note):
+    notes.append({
+        "title": request.title,
+        "content": request.content,
+        "done": False
+    })
+
+@app.get("/notes", response_model=list[Note])
+async def read_notes():
+    return notes
+
+@app.get("/notes/{id}")
+async def read_note(id: int):
+    return notes[id-1]
+
+@app.put("/notes/{id}")
+async def update_note(id: int, request: NoteUpdate):
+    notes[id-1]["content"] = request.content
+
+@app.delete("/notes/{id}")
+async def delete_note(id: int):
+    notes.remove(notes[id-1])
